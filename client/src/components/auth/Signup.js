072a77401af3308f8a37051311a37b9373ff2af7
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-11 15:59:01 -0500

    import and revise redirect url with an environment variable

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { auth } from "../../api/firebase/firebase.js";
+import { auth } from "../../api/firebase/firebaseConfig";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const REDIRECT_URL = process.env.REACT_APP_SIGNUP_REDIRECT_URL;
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -12,7 +14,7 @@ const Signup = () => {
   const handleOnSubmit = async (e) => {
     e.preventDefault();
     const config = {
-      url: "http://localhost:3000/signup/complete",
+      url: REDIRECT_URL,
       handleCodeInApp: true,
     };
     await auth.sendSignInLinkToEmail(email, config);
@@ -24,7 +26,7 @@ const Signup = () => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h3>Signup.</h3>
+          <h3>Sign Up.</h3>
           <ToastContainer />
           <form onSubmit={handleOnSubmit}>
             <input
