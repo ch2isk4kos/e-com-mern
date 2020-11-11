b544a522a0423db5956c525906da560dc54810e6
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-11 02:05:05 -0500

    import and add route for signup component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/nav/Header";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
+import Signup from "./components/auth/Signup";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path={"/"} component={Landing} />
         <Route exact path={"/home"} component={Home} />
+        <Route exact path={"/signup"} component={Signup} />
       </Switch>
     </div>
   );
