088e2169575eafd698213203b71c5df660dbd058
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-11 00:18:40 -0500

    comment strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
