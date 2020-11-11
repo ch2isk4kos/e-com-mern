432ce1924b3e9ba55e336d84d4454d59d1a83f22
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-10 21:10:21 -0500

    import home and landing page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Landing from './components/Landing';
+import Home from './components/Home';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Switch>
+        <Route exact path={'/'} component={Landing} />
+        <Route exact path={'/home'} component={Home} />
+      </Switch>
     </div>
   );
 }
