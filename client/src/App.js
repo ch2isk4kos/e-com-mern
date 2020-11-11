6ee709acfd40b635d07cb861190b42dcc95ba5fd
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-10 23:17:29 -0500

    import and render navigation header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Landing from './components/Landing';
-import Home from './components/Home';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import Header from "./components/nav/Header";
+import Landing from "./components/Landing";
+import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Switch>
-        <Route exact path={'/'} component={Landing} />
-        <Route exact path={'/home'} component={Home} />
+        <Route exact path={"/"} component={Landing} />
+        <Route exact path={"/home"} component={Home} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
