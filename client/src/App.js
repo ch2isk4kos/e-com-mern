1a29417a38149e28227e4db2bd1472fdcdd9d00a
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-11 15:59:41 -0500

    import toast container component and add file path to toasty css file

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Landing from "./components/Landing";
 import Home from "./components/Home";
 import Signup from "./components/auth/Signup";
 import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 const App = () => {
