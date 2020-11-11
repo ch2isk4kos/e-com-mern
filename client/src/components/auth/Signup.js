6a1ca484d1b7de6572d94df649e897ed55eb14d9
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-11 02:04:19 -0500

    create signup component and add signup form

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h3>Signup.</h3>
+          <form onSubmit={handleOnSubmit}>
+            <input
+              className="form-control"
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={handleOnChange}
+              autoFocus
+            />
+            <button className="btn btn-raised" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
