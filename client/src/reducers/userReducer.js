1e8dc06674c12f805c4d66e1a94c5f2fc4919f67
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-12 02:22:00 -0500

    change action type name

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -2,7 +2,7 @@ export const userReducer = (state = null, action) => {
   switch (action.type) {
     case "USER_LOGIN":
       return action.payload; // user = { email: "", password: "", cart: []}
-    case "USER_LOGOUT":
+    case "USER_SIGNOUT":
       return action.payload; // user = { null }
     default:
       return state;
