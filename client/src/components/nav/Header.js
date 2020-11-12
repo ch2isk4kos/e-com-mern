33ab1fcc714155d0157b650ff95e25322dec3618
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-12 02:23:13 -0500

    add items with icons to navigation

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
 import {
+  AppstoreOutlined,
+  AuditOutlined,
   BookOutlined,
   BuildOutlined,
   FireOutlined,
@@ -13,14 +15,28 @@ import {
   UserOutlined,
   WalletOutlined,
 } from "@ant-design/icons";
+import firebase from "firebase/firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
+// ant design
 const { SubMenu, Item, ItemGroup } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("");
-
+  const dispatch = useDispatch();
+  const history = useHistory();
   const handleOnClick = (e) => {
     setCurrent({ current: e.key });
+  };
+  const handleOnSignOut = () => {
+    firebase.auth().signOut();
+    // update store
+    dispatch({
+      type: "USER_SIGNOUT",
+      payload: null,
+    });
+    history.push("/");
   };
 
   return (
@@ -29,9 +45,13 @@ const Header = () => {
       <Item key="thunderbolt" icon={<FireOutlined />}>
         <Link to={"/home"}>Yard Sale</Link>
       </Item>
+      {/* Categories */}
+      <Item key="categories" icon={<AppstoreOutlined />}>
+        <Link to={"/categories"}>Categories</Link>
+      </Item>
       {/* Products */}
       <Item key="products" icon={<TagsOutlined />}>
-        <Link to={"/products"}>Products</Link>
+        <Link to={"/products"}>New Products</Link>
       </Item>
       {/* Login */}
       <Item key="login" icon={<LoginOutlined />}>
@@ -44,16 +64,23 @@ const Header = () => {
       {/* Profile */}
       <SubMenu
         className="float-right"
-        key="profile"
+        key="account"
         icon={<UserOutlined />}
-        title="Profile"
+        title="Account"
       >
         {/* Profile Sub Menu */}
         <ItemGroup title="Menu">
+          <Item key="profile" icon={<AuditOutlined />}>
+            Profile
+          </Item>
           <Item key="settings" icon={<SettingOutlined />}>
             Settings
           </Item>
-          <Item key="signout" icon={<SelectOutlined />}>
+          <Item
+            key="signout"
+            icon={<SelectOutlined />}
+            onClick={handleOnSignOut}
+          >
             Sign Out
           </Item>
         </ItemGroup>
