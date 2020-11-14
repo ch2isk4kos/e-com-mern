c98644c0cf8d5f773fab434cebf9f9ae9cfc7a05
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-14 01:18:13 -0500

    add try catch for to receive request from client

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,14 @@
 const admin = require("../firebase");
 
-exports.authenticate = (req, res, next) => {
-  console.log(req.headers); // token
-  next();
+exports.authenticate = async (req, res, next) => {
+  console.log("HEADERS:", req.headers); // token
+  try {
+    const firebaseUser = await admin.auth().verifyIdToken(req.headers.auth);
+    req.user = firebaseUser;
+    console.log("FIREBASE USER AUTHENTICATION:", req.user);
+    next();
+  } catch (err) {
+    res.status(401).json({ err: "Invalid or Expired Token" });
+    console.log(`Authentication Error: ${err}`);
+  }
 };
