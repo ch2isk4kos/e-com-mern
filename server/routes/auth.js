f0b8f1e3bd32c7b94eaba8e843f545cda30656c6
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-13 21:55:04 -0500

    import authentication controller and pass as second parameter of get request

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/create-or-update-user", (req, res) => {
-  res.json({
-    data: "Create or Update User Endpoint.",
-  });
-});
+// authentication controller
+const { createOrUpdateUser } = require("../controllers/auth");
+
+router.get("/create-or-update-user", createOrUpdateUser);
 
 module.exports = router;
