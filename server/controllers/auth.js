4f3866acb5e0ec86d02204ddcc0c26862180e9cf
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-14 02:41:18 -0500

    import user model and add functionality to create or update a user instance in firebase

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,23 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "Create or Update User.",
-  });
+const User = require("../models/User");
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+  const user = await User.findOneAndUpdate(
+    { email: email }, // find user based on email
+    { name: name, picture: picture }, // update user name and picture
+    { new: true } // returns updated user object
+  );
+
+  if (user) {
+    res.json(user);
+    console.log("USER UPDATED:", user);
+  } else {
+    const newUser = await new User({
+      email: email,
+      name: name,
+      picture: picture,
+    }).save();
+    res.json(newUser);
+    console.log("NEW USER CREATED:", newUser);
+  }
 };
