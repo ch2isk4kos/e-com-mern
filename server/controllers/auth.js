bc2459ea7b557b26984822f14b0031781a4e6192
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-14 23:39:00 -0500

    revise syntax

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 exports.createOrUpdateUser = async (req, res) => {
   const { name, picture, email } = req.user;
+
   const user = await User.findOneAndUpdate(
     { email: email }, // find user based on email
     { name: name, picture: picture }, // update user name and picture
@@ -9,15 +10,15 @@ exports.createOrUpdateUser = async (req, res) => {
   );
 
   if (user) {
-    res.json(user);
     console.log("USER UPDATED:", user);
+    res.json(user);
   } else {
     const newUser = await new User({
       email: email,
       name: name,
       picture: picture,
     }).save();
-    res.json(newUser);
     console.log("NEW USER CREATED:", newUser);
+    res.json(newUser);
   }
 };
