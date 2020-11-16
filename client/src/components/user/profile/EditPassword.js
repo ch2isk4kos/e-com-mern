beef9391f08486003c6b3c44eedd8d079c72f9b6
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 01:52:44 -0500

    create component to edit user password

--- client/src/components/user/profile/EditPassword.js
+++ client/src/components/user/profile/EditPassword.js
@@ -1,30 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileNav from "./ProfileNav";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { auth } from "../../../api/firebase/firebaseConfig";
+import { toast } from "react-toastify";
 
-const EditPassword = () => {
+const EditPassword = ({ history }) => {
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  // const editPasswordForm = () => <form><</form>
+
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await auth.currentUser
+      .updatePassword(password)
+      .then(() => {
+        setIsLoading(false);
+        setPassword("");
+        toast.success("You've successfully updated your password.");
+        history.push("/user/profile");
+      })
+      .catch((err) => {
+        setIsLoading(true);
+        toast.error(err.message);
+      });
+  };
+
   return (
-    <div className="container p-5">
+    <div className="container-fluid">
       <div className="row">
-        <div className="col-md-6 offset-md-3">
-          {/* {loading ? (
-            <h3 className="text-danger">Logging In...</h3>
+        <div className="col-md-2">
+          <ProfileNav />
+        </div>
+        <div className="col">
+          {isLoading ? (
+            <h1 className="text-danger">Loading...</h1>
           ) : (
-            <h3>Login.</h3>
-          )} */}
-          <form>
-            <h3>Edit Password</h3>
-            <input
-              className="form-control"
-              type="password"
-              placeholder="enter new password"
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-              autoFocus
-            />
-            <button type="submit">Edit</button>
-            <br />
-            <Link to="/password-reset">Forgot My Password</Link>
+            <h1 className="text-primary">Edit Password</h1>
+          )}
+          <form onSubmit={handleOnSubmit}>
+            <div className="form-group">
+              <div className="col-md-6 offset-md-3">
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="enter new password"
+                  value={password}
+                  disabled={isLoading}
+                  onChange={handleOnChange}
+                  autoFocus
+                />
+                <button
+                  type="submit"
+                  className="btn btn-primary m-3"
+                  disabled={!password || password.length < 6 || isLoading}
+                >
+                  Update Password
+                </button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
