c06d9dbc57ae9d469fefa54e27e25d3327dffd32
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 18:08:08 -0500

    create file and add routes for category endpoints

---
+++ server/routes/category.js
@@ -0,0 +1,27 @@
+// create router
+const express = require("express");
+const router = express.Router();
+
+// middleware imports
+const {
+  authenticateToken,
+  authenticateAdmin,
+} = require("../middleware/auth.js");
+
+// authentication controller imports
+const {
+  index,
+  create,
+  read,
+  update,
+  remove,
+} = require("../controllers/category.js");
+
+// Category Routes
+router.get("/categories", index);
+router.post("/category", authenticateToken, authenticateAdmin, create);
+router.get("/category/:slug", authenticateToken, authenticateAdmin, read);
+router.put("/category/:slug", authenticateToken, authenticateAdmin, update);
+router.delete("/category/:slug", authenticateToken, authenticateAdmin, remove);
+
+module.exports = router;
