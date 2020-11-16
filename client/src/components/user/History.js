9003dbdc815e7aa46dcc6b1137d8568cb3fe840d
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-15 23:28:11 -0500

    import and render user nav component

--- client/src/components/user/History.js
+++ client/src/components/user/History.js
@@ -1,9 +1,13 @@
 import React from "react";
+import UserNav from "./UserNav";
 
 const History = () => {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
         <div className="col">
           <h1>User History</h1>
         </div>
