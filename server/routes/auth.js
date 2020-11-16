29fca92567e58b2000222c9f36a4d265b01b39df
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 02:15:37 -0500

    import admin authentication middlware and add route for current admin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,26 @@
+// create router
 const express = require("express");
 const router = express.Router();
 
-// middleware
-const { authenticateToken } = require("../middleware/auth.js");
+// middleware imports
+const {
+  authenticateToken,
+  authenticateAdmin,
+} = require("../middleware/auth.js");
 
-// authentication controller
+// authentication controller imports
 const { createOrUpdateUser, currentUser } = require("../controllers/auth.js");
 
+// User Routes
 router.post("/create-or-update-user", authenticateToken, createOrUpdateUser);
 router.post("/current-user", authenticateToken, currentUser);
+
+// Admin Routes
+router.post(
+  "/current-admin",
+  authenticateToken,
+  authenticateAdmin,
+  createOrUpdateUser
+);
 
 module.exports = router;
