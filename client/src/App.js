00755fc01c04ff0cb6931f868d3ba4231f476fd4
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 04:02:12 -0500

    import and add routee for admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Header from "./components/nav/Header";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
@@ -7,13 +8,14 @@ import SignupVerified from "./components/auth/SignupVerified";
 import Login from "./components/auth/Login";
 import PasswordReset from "./components/auth/PasswordReset";
 import UserRoute from "./components/routes/userRoute";
+import AdminRoute from "./components/routes/adminRoute";
 import History from "./components/user/History";
 import Main from "./components/user/profile/Main";
 import EditPassword from "./components/user/profile/EditPassword";
 import Cart from "./components/user/Cart";
 import Wishlist from "./components/user/Wishlist";
+import AdminDashboard from "./components/admin/AdminDashboard";
 import { Switch, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { auth } from "./api/firebase/firebaseConfig";
 import { currentUser } from "./api/firebase/firebaseFunctions";
 import { ToastContainer } from "react-toastify";
@@ -47,7 +49,7 @@ const App = () => {
       }
     });
     return () => unsubscribe();
-  });
+  }, [dispatch]);
   return (
     <div className="App">
       <Header />
@@ -59,14 +61,22 @@ const App = () => {
         <Route exact path={"/signup/verified"} component={SignupVerified} />
         <Route exact path={"/login"} component={Login} />
         <Route exact path={"/password-reset"} component={PasswordReset} />
-        {/* Custom */}
-        <UserRoute>
-          <Route exact path={"/user/history"} component={History} />
-          <Route exact path={"/user/cart"} component={Cart} />
-          <Route exact path={"/user/wishlist"} component={Wishlist} />
-          <Route exact path={"/user/profile"} component={Main} />
-          <Route exact path={"/user/edit-password"} component={EditPassword} />
-        </UserRoute>
+        {/* User Routes*/}
+        <UserRoute exact path={"/user/history"} component={History} />
+        <UserRoute exact path={"/user/cart"} component={Cart} />
+        <UserRoute exact path={"/user/wishlist"} component={Wishlist} />
+        <UserRoute exact path={"/user/profile"} component={Main} />
+        <UserRoute
+          exact
+          path={"/user/edit-password"}
+          component={EditPassword}
+        />
+        {/* Admin Routes */}
+        <AdminRoute
+          exact
+          path={"/admin/dashboard"}
+          component={AdminDashboard}
+        />
       </Switch>
     </div>
   );
