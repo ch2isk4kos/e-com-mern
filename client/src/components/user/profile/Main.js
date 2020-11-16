7ffbe23bcc19281e7e38b78dcd375c54d3bb3028
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 01:54:23 -0500

    add structure to user profile container

--- client/src/components/user/profile/Main.js
+++ client/src/components/user/profile/Main.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import EditPassword from "./EditPassword";
+import ProfileNav from "./ProfileNav";
 
 const Main = () => {
   return (
-    <div>
-      <Link to={"/user/edit-password"}>Edit Password</Link>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <ProfileNav />
+        </div>
+        <div className="col">
+          <h1>User Profile</h1>
+        </div>
+      </div>
     </div>
   );
 };
