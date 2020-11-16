75f8f6adfdb96c0bce1787513e5791bc880b208b
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 18:11:32 -0500

    create file and add export templates for category controllers

---
+++ server/controllers/category.js
@@ -0,0 +1,11 @@
+const Category = require("../models/Category");
+
+exports.index = (req, res) => {};
+
+exports.create = (req, res) => {};
+
+exports.read = (req, res) => {};
+
+exports.update = (req, res) => {};
+
+exports.remove = (req, res) => {};
