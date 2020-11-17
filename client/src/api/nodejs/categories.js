2dd40a51f90c15c607bd27a3f9520641f5d9215c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 23:45:01 -0500

    create file for nodejs server methods and add CRUD pattern

---
+++ client/src/api/nodejs/categories.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const NODE_API = process.env.REACT_APP_NODE_API_URL;
+
+// index
+export const getCategories = async () => {
+  return await axios.get(`${NODE_API}/categories`);
+};
+
+// creete
+export const createCategory = async (category, token) => {
+  return await axios.post(`${NODE_API}/category`, category, {
+    headers: {
+      auth: token,
+    },
+  });
+};
+
+// read
+export const getCategory = async (slug) => {
+  return await axios.get(`${NODE_API}/category/${slug}`);
+};
+
+// update
+export const updateCategory = async (slug, category, token) => {
+  return await axios.put(
+    `${NODE_API}/category/${slug}`,
+    {
+      name: `${category}`,
+    },
+    {
+      headers: {
+        auth: token,
+      },
+    }
+  );
+};
+
+// delete
+export const removeCategory = async (slug, token) => {
+  return await axios.delete(
+    `${NODE_API}/category/${slug}`,
+    {},
+    {
+      headers: {
+        auth: token,
+      },
+    }
+  );
+};
