91922eeb97bac72f961e4803a6a97e7be9927816
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-16 21:44:26 -0500

    add middleware export for admin to create an instance of category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,11 +1,31 @@
 const Category = require("../models/Category");
+const slugify = require("slugify");
 
-exports.index = (req, res) => {};
+exports.index = async (req, res) => {};
 
-exports.create = (req, res) => {};
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    // const category = await new Category({
+    //   name: name,
+    //   slug: slugify(name),
+    // })
+    //   .toLowerCase()
+    //   .save();
+    // res.json(category)
+    res.json(
+      await new Category({
+        name: name,
+        slug: slugify(name),
+      }).save()
+    );
+  } catch (err) {
+    res.status(400).send("Error with creating category");
+  }
+};
 
-exports.read = (req, res) => {};
+exports.read = async (req, res) => {};
 
-exports.update = (req, res) => {};
+exports.update = async (req, res) => {};
 
-exports.remove = (req, res) => {};
+exports.remove = async (req, res) => {};
