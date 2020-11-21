ff3b46eb0fe3cd6a5df8774acbea0d6c8806337c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-20 20:37:35 -0500

    add component to abstract product create form

---
+++ client/src/components/admin/product/ProductForm.js
@@ -0,0 +1,123 @@
+import React from "react";
+
+const ProductForm = ({ product, handleOnChange, handleOnSubmit }) => {
+  const {
+    name,
+    price,
+    description,
+    colors,
+    color,
+    brands,
+    brand,
+    images,
+    categories,
+    category,
+    subcategories,
+    shipping,
+    quantity,
+  } = product;
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div className="form-group">
+        <div className="col-md-6 offset-md-3">
+          <>
+            <label className="float-left">Product Name</label>
+            <input
+              className="form-control mb-2"
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleOnChange}
+              autoFocus
+              required
+            />
+          </>
+          <>
+            <label className="float-left">Price</label>
+            <input
+              className="form-control mb-2"
+              type="number"
+              name="price"
+              value={price}
+              onChange={handleOnChange}
+              required
+            />
+          </>
+          <>
+            <label className="float-left">Color</label>
+            <select
+              className="form-control mb-2"
+              name="color"
+              onChange={handleOnChange}
+            >
+              <option></option>
+              {colors.map((c) => (
+                <option key={c} value={c}>
+                  {c}
+                </option>
+              ))}
+            </select>
+          </>
+          {/* <select id="" name="category">
+                  <option value={category}>Select Category</option>
+                </select> */}
+          <>
+            <label className="float-left">Brand</label>
+            <select
+              className="form-control mb-2"
+              name="brand"
+              onChange={handleOnChange}
+            >
+              <option></option>
+              {brands.map((b) => (
+                <option key={b} value={b}>
+                  {b}
+                </option>
+              ))}
+            </select>
+          </>
+          <>
+            <label className="float-left">Shipping</label>
+            <select
+              className="form-control mb-2"
+              name="shipping"
+              onChange={handleOnChange}
+            >
+              <option></option>
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </select>
+          </>
+          <>
+            <label className="float-left">Quantity</label>
+            <input
+              className="form-control mb-2"
+              type="number"
+              name="quantity"
+              value={quantity}
+              onChange={handleOnChange}
+            />
+          </>
+          <>
+            <label className="float-left">Description</label>
+            <textarea
+              className="form-control mb-2"
+              type="text"
+              rows={5}
+              cols={5}
+              name="description"
+              value={description}
+              onChange={handleOnChange}
+            />
+          </>
+
+          <button className="btn btn-primary m-3" type="submit">
+            Create
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ProductForm;
