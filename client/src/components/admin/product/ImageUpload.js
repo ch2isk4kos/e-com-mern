c297a0087ab73681a4174fe218ab959491623dbc
Workbench <workbench@Skybox.fios-router.home>
2020-11-30 22:33:00 -0500

    resize uploaded images and invoke api action method to store data in redux state

--- client/src/components/admin/product/ImageUpload.js
+++ client/src/components/admin/product/ImageUpload.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import { uploadImages } from "../../../api/cloudinary/images";
+import { useSelector } from "react-redux";
 
-const ImageUpload = () => {
+const ImageUpload = ({ product, setProduct, isLoading, setIsLoading }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   const handleOnUpload = (e) => {
     console.log("Files: ", e.target.files);
-    // resize
+
+    // resize images
+    let files = e.target.files;
+    let productImages = product.images;
+
+    if (files) {
+      setIsLoading(true);
+
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720, // width
+          720, // height
+          "JPG", // compress format
+          100, // quality
+          0, // rotation
+          // vvv response uri function vvv
+          (uri) => {
+            console.log("URI: ", uri);
+            const token = user ? user.token : "";
+            uploadImages(uri, token)
+              .then((res) => {
+                setIsLoading(false);
+                productImages.push(res.data);
+                setProduct({ ...product, images: productImages });
+              })
+              .catch((err) => {
+                setIsLoading(true);
+                console.log("CLOUDINARY UPLOAD", err);
+              });
+          },
+          "base64" // output type
+        );
+      }
+    }
 
     // send to mongo atlas which will upload images to cloudinary
 
