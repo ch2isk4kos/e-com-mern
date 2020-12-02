6687c3a15aa0750c6b33891fba67ce58cc7c74a3
Workbench <workbench@Skybox.fios-router.home>
2020-12-02 16:41:16 -0500

    add method to fetch and set the number of products specified to api server method

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,11 +5,20 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const loadProducts = () => {
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+    });
+  };
 
   return (
     <div>
       <h1>Home</h1>
+      {JSON.stringify(products)}
     </div>
   );
 };
