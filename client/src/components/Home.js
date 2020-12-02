5107fd1e4139beb993efcfadd20f5a4cea954bbb
Workbench <workbench@Skybox.fios-router.home>
2020-12-02 16:37:48 -0500

    add state and useEffect template to home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { getProducts, getProductsByCount } from "../api/nodejs/products";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <h1>Home</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
