c7cceb523b1e067ec853979d6987c8edc2422d7e
Workbench <workbench@Skybox.fios-router.home>
2020-12-01 23:52:31 -0500

    add and pre populate form to update product instance

--- client/src/components/admin/product/ProductForm.js
+++ client/src/components/admin/product/ProductForm.js
@@ -143,7 +143,6 @@ const ProductForm = ({
             <label className="float-left">Quantity</label>
             <input
               className="form-control mb-2"
-              type="number"
               name="quantity"
               value={quantity}
               onChange={handleOnChange}
