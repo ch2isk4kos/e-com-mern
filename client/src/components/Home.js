5a30ac9670f376435e171f30d40c12135d7b7604
Workbench <workbench@Skybox.fios-router.home>
2020-12-02 16:55:08 -0500

    apply structure to home component render and pass props to product card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ProductCard from "../components/admin/product/ProductCard";
 import { getProducts, getProductsByCount } from "../api/nodejs/products";
 
 const Home = () => {
@@ -10,16 +11,30 @@ const Home = () => {
   }, []);
 
   const loadProducts = () => {
+    setIsLoading(true);
     getProductsByCount(3).then((res) => {
       setProducts(res.data);
+      setIsLoading(false);
     });
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className="jumbotron">
+        <h1>Home</h1>
+        {isLoading ? <h3>Loading</h3> : <h3>Shop Products</h3>}
+        {/* {JSON.stringify(products)} */}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((p) => (
+            <div className="col-md-4" key={p._id}>
+              <ProductCard product={p} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
