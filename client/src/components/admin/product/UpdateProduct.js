32e542163b7bd3ff6ab4900688db47cf32094305
Workbench <workbench@Skybox.fios-router.home>
2020-12-01 23:05:28 -0500

    import state and api server method to fetch product instance to update

--- client/src/components/admin/product/UpdateProduct.js
+++ client/src/components/admin/product/UpdateProduct.js
@@ -1,22 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import ProductForm from "./ProductForm";
+import AdminNav from "../AdminNav";
 import {
   getCategories,
   getSubCategories,
 } from "../../../api/nodejs/categories";
-import {
-  getProducts,
-  createProduct,
-  removeProduct,
-} from "../../../api/nodejs/products";
+import { getProduct, updateProduct } from "../../../api/nodejs/products";
 import { toast } from "react-toastify";
 import { LoadingOutlined } from "@ant-design/icons";
-import AdminNav from "../AdminNav";
-import ProductForm from "./ProductForm";
 
-const UpdateProduct = () => {
+const initState = {
+  name: "",
+  price: "",
+  description: "",
+  colors: ["Black", "Blue", "Green", "Grey", "Red", "Yellow"],
+  color: "",
+  brands: ["Apple", "Asus", "Microsoft"],
+  brand: "",
+  images: [],
+  categories: [],
+  category: "",
+  subcategories: [],
+  shipping: "",
+  quantity: "",
+  purchased: "",
+};
+
+const UpdateProduct = ({ match }) => {
   const { user } = useSelector((state) => ({ ...state }));
+  const [product, setProduct] = useState(initState);
+  const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug)
+      .then((res) => {
+        setProduct({ ...product, ...res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="container-fluid">
@@ -24,9 +53,10 @@ const UpdateProduct = () => {
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">
+        <div className="col-md-10">
           <h1>Update Product</h1>
           {/* Update Product Form */}
+          {JSON.stringify(product)}
         </div>
       </div>
     </div>
