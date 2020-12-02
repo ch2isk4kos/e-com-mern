343330e02b489b0730247882f6e0c7d5874e43d0
Workbench <workbench@Skybox.fios-router.home>
2020-12-02 17:46:43 -0500

    add props to render in product card component

--- client/src/components/admin/product/ProductCard.js
+++ client/src/components/admin/product/ProductCard.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "antd";
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import logo from "../../../assets/yard-sale.jpg";
+
+const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
-  return <div>{product.name}</div>;
+  const { name, description, images, slug } = product;
+  const src = images && images.length ? images[0].url : logo;
+  const desc = description && description.substring(0, 50);
+
+  return (
+    <div>
+      <Card
+        cover={
+          <img
+            className=""
+            src={src}
+            alt={"product"}
+            style={{ height: "300px" }}
+          />
+        }
+        actions={[
+          <Link to={`/product/${slug}`}>
+            <EyeOutlined className="text-warning" />
+            <br />
+            <p>View Product</p>
+          </Link>,
+          <>
+            <ShoppingCartOutlined className="text-danger" />
+            <br />
+            <p>Add to Cart</p>
+          </>,
+        ]}
+      >
+        <Meta title={name} description={`${desc}...`} />
+      </Card>
+    </div>
+  );
 };
 
 export default ProductCard;
