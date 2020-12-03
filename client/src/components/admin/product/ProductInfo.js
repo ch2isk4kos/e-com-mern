6f926decf97e91406c2db7f39bb11dc7d4326b9a
Workbench <workbench@Skybox.fios-router.home>
2020-12-03 18:07:16 -0500

    add dynamic data for product instance

--- client/src/components/admin/product/ProductInfo.js
+++ client/src/components/admin/product/ProductInfo.js
@@ -1,7 +1,76 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ProductInfo = () => {
-  return <div></div>;
+const ProductInfo = ({ product }) => {
+  const {
+    price,
+    brand,
+    color,
+    shipping,
+    quantity,
+    purchased,
+    category,
+    subcategories,
+  } = product;
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">
+        <span className="float-left">Price</span>
+        <span className="label label-default label-pill pull-xs-right">
+          <span className="float-right">${price}</span>
+        </span>
+      </li>
+      <li className="list-group-item">
+        <span className="float-left">Brand</span>
+        <span className="label label-default label-pill pull-xs-right">
+          <span className="float-right">{brand}</span>
+        </span>
+      </li>
+      <li className="list-group-item">
+        <span className="float-left">Color</span>
+        <span className="label label-default label-pill pull-xs-right">
+          <span className="float-right">{color}</span>
+        </span>
+      </li>
+      <li className="list-group-item">
+        <span className="float-left">Shipping</span>
+        <span className="label label-default label-pill pull-xs-right">
+          <span className="float-right">{shipping}</span>
+        </span>
+      </li>
+      <li className="list-group-item">
+        <span className="float-left">In Stock</span>
+        <span className="label label-default label-pill pull-xs-right">
+          <span className="float-right">{quantity}</span>
+        </span>
+      </li>
+      {category && (
+        <li className="list-group-item">
+          <span className="float-left">Category</span>
+          <Link
+            to={`/cateogory/${category.slug}`}
+            className="label label-default label-pill pull-xs-right"
+          >
+            <span className="float-right">{category.name}</span>
+          </Link>
+        </li>
+      )}
+      {subcategories && (
+        <li className="list-group-item">
+          <span className="float-left">Sub Categories</span>
+          {subcategories.map((sub) => (
+            <Link
+              to={`/sub/${sub.slug}`}
+              key={sub._id}
+              className="label label-default label-pill pull-xs-right"
+            >
+              <span className="float-right">{sub.name}</span>
+            </Link>
+          ))}
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default ProductInfo;
