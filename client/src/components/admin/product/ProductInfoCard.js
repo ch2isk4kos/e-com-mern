21d6acba8300d9efab7d04c40df9de4d511d4db4
Workbench <workbench@Skybox.fios-router.home>
2020-12-03 18:15:44 -0500

    import and render tabs for description and comment sections

--- client/src/components/admin/product/ProductInfoCard.js
+++ client/src/components/admin/product/ProductInfoCard.js
@@ -2,27 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ProductInfo from "./ProductInfo";
 import logo from "../../../assets/yard-sale.jpg";
-import { Card } from "antd";
+import { Card, Descriptions, Tabs } from "antd";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const { Meta } = Card;
+const { TabPane } = Tabs;
 
 const ProductInfoCard = ({ product }) => {
-  const { name, images } = product;
+  const { name, images, description } = product;
   return (
     <>
       <div className="col-md-7">
-        {images && images.length ? (
-          <Carousel showArrows={true} autoPlay infiniteLoop>
-            {images.map((image) => (
-              <img key={image.public_id} src={image.url} />
-            ))}
-          </Carousel>
-        ) : (
-          <Card className="card-image mb-3" cover={<img src={logo} />}></Card>
-        )}
+        <>
+          {images && images.length ? (
+            <Carousel showArrows={true} autoPlay infiniteLoop>
+              {images.map((image) => (
+                <img key={image.public_id} src={image.url} />
+              ))}
+            </Carousel>
+          ) : (
+            <Card className="card-image mb-3" cover={<img src={logo} />}></Card>
+          )}
+        </>
+        <>
+          <Tabs type="card">
+            <TabPane key="1" tab="Description">
+              {description}
+            </TabPane>
+            <TabPane key="2" tab="Comments">
+              Leave a Comment
+            </TabPane>
+          </Tabs>
+        </>
       </div>
       <div className="col-md-5">
         <h1 className="p-3" style={{ background: "WhiteSmoke" }}>
@@ -33,7 +46,7 @@ const ProductInfoCard = ({ product }) => {
             <Link to={`/`}>
               <HeartOutlined className="text-warning" />
               <br />
-              <p>Like</p>
+              <p>Add to Wishlist</p>
             </Link>,
             <>
               <ShoppingCartOutlined className="text-danger" />
