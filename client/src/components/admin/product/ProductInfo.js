0f4ea2d173adcddc894aaa230d0800e7e8435cad
Workbench <workbench@Skybox.fios-router.home>
2020-12-03 00:40:31 -0500

    render image carousel and pass props for product imagees

--- client/src/components/admin/product/ProductInfo.js
+++ client/src/components/admin/product/ProductInfo.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const { Meta } = Card;
 
@@ -9,7 +11,14 @@ const ProductInfo = ({ product }) => {
   const { name, description, images, slug } = product;
   return (
     <>
-      <div className="col-md-7">Image Carousel</div>
+      <div className="col-md-7">
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images &&
+            images.map((image) => (
+              <img key={image.public_id} src={image.url} />
+            ))}
+        </Carousel>
+      </div>
       <div className="col-md-5">
         <Card
           actions={[
