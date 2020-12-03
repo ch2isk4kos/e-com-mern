38efa644f897bc21d8b984b7a4f35acd10d0c4ec
Workbench <workbench@Skybox.fios-router.home>
2020-12-03 18:33:44 -0500

    add server endpoint for product instance rating

--- server/routes/product.js
+++ server/routes/product.js
@@ -12,21 +12,28 @@ const {
 const {
   index,
   list,
+  tally,
   create,
   read,
   update,
   remove,
-  tally,
+  rating,
 } = require("../controllers/product");
 
 // Category Routes
+// index
 router.get("/products", index);
 router.post("/products/list", list);
 router.get("/products/tally", tally);
 router.get("/products/:count", index);
+// c
 router.post("/product", authenticateToken, authenticateAdmin, create);
+// r
 router.get("/product/:slug", read);
+// u
 router.put("/product/:slug", authenticateToken, authenticateAdmin, update);
+// d
 router.delete("/product/:slug", authenticateToken, authenticateAdmin, remove);
-
+// ratings
+router.put("/product/:id/rating", authenticateToken, rating);
 module.exports = router;
