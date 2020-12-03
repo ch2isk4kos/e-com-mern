726e3bf51236357439d2be938726132775472e9d
Workbench <workbench@Skybox.fios-router.home>
2020-12-02 23:47:58 -0500

    create product component with state and fetch api server endpoint for instance

---
+++ client/src/components/admin/product/Product.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { getProduct } from "../../../api/nodejs/products";
+
+const Product = ({ match }) => {
+  const [product, setProduct] = useState({});
+  const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, [slug]);
+
+  const loadProduct = () => {
+    getProduct(slug).then((res) => setProduct(res.data));
+  };
+
+  return <div>{JSON.stringify(product)}</div>;
+};
+
+export default Product;
