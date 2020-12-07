34f01cbfd0dad2aceab30e1f4fcf654d787d89f3
Workbench <workbench@Skybox.fios-router.home>
2020-12-06 21:25:57 -0500

    create search component and render template

---
+++ client/src/components/search/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { SearchOutlined } from "@ant-design/icons";
+
+const Search = () => {
+  return <div></div>;
+};
+
+export default Search;
