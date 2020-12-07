c37ee183e670b06d5134a52944722a43351e2054
Workbench <workbench@Skybox.fios-router.home>
2020-12-06 21:36:56 -0500

    render search input component and pass props to handle on input key stroke and on query submission

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -4,7 +4,34 @@ import { useSelector, useDispatch } from "react-redux";
 import { SearchOutlined } from "@ant-design/icons";
 
 const Search = () => {
-  return <div></div>;
+  let dispatch = useDispatch();
+  const { search } = useSelector((state) => ({ ...state }));
+  const { text } = search;
+  const { history } = useHistory();
+
+  const handleOnChange = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: e.target.value },
+    });
+  };
+  const handleOnCSubmit = () => {
+    e.preventDefault();
+    history.push(`/shop?${text}`);
+  };
+
+  return (
+    <form className="form-inline my-2 my-lg-0" onSubmit={handleOnSubmit}>
+      <input
+        className="form-control mr-sm-2"
+        type="search"
+        placeholder="search"
+        value={text}
+        onChange={handleOnChange}
+      />
+      <SearchOutlined onClick={handleOnSubmit} style={{ cursor: "pointer" }} />
+    </form>
+  );
 };
 
 export default Search;
