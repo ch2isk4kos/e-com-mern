113b996c921cc621ea281d8e72e5c098e36a9760
Workbench <workbench@Skybox.fios-router.home>
2020-12-09 17:01:37 -0500

    add functionality to store cart in redux and local storage when user clicks add to cart

--- client/src/components/admin/product/ProductInfoCard.js
+++ client/src/components/admin/product/ProductInfoCard.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import ProductInfo from "./ProductInfo";
 import RatingModal from "./RatingModal";
 import { averageRating } from "../../../api/custom/ratings";
 import logo from "../../../assets/yard-sale.jpg";
 import StarRating from "react-star-ratings";
-import { Card, Tabs } from "antd";
+import { Card, Tabs, Tooltip } from "antd";
 import {
   HeartOutlined,
   StarOutlined,
@@ -13,12 +14,48 @@ import {
 } from "@ant-design/icons";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import _ from "lodash";
 
 // const { Meta } = Card;
 const { TabPane } = Tabs;
 
 const ProductInfoCard = ({ product, rating, handleOnRatingSelection }) => {
   const { _id, name, images, description } = product;
+
+  const { user, cart } = useSelector((state) => ({ ...state }));
+  const [tooltip, setTooltip] = useState("Click to Add");
+  const dispatch = useDispatch();
+
+  const handleOnAddToCart = () => {
+    // create cart array
+    let cart = [];
+
+    if (typeof window !== "undefined") {
+      // get data from local storage
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      // push product(s) to cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // remove duplicates with lodash
+      let unique = _.uniqWith(cart, _.isEqual);
+      // save to local storage
+      localStorage.setItem("cart", JSON.stringify(unique));
+      // display ant design tooltip
+      setTooltip("Added");
+      // add to redux state
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+    //
+    console.log("");
+  };
+
   return (
     <>
       <div className="col-md-7">
@@ -59,9 +96,13 @@ const ProductInfoCard = ({ product, rating, handleOnRatingSelection }) => {
               <p>Add to Wishlist</p>
             </Link>,
             <>
-              <ShoppingCartOutlined className="text-danger" />
-              <br />
-              <p>Add to Cart</p>
+              <Tooltip title={tooltip}>
+                <a onClick={handleOnAddToCart}>
+                  <ShoppingCartOutlined className="text-danger" />
+                  <br />
+                  <p>Add to Cart</p>
+                </a>
+              </Tooltip>
             </>,
             <RatingModal>
               <StarRating
