5a45384139faf37e9731dd3384e21ce34ea5f154
Workbench <workbench@Skybox.fios-router.home>
2020-12-09 22:27:09 -0500

    create component for cart and render temaplate

---
+++ client/src/components/cart/Cart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <h4>Cart</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
