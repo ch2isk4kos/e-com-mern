08687eb3ee6ce83451cd702e389980a359740d25
Workbench <workbench@Skybox.fios-router.home>
2020-12-10 22:27:14 -0500

    add items from cart to drawer component

--- client/src/components/cart/CartDrawer.js
+++ client/src/components/cart/CartDrawer.js
@@ -2,14 +2,73 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Drawer, Button } from "antd";
+import logo from "../../assets/yard-sale.jpg";
 
-const CartDrawer = ({ children }) => {
+const CartDrawer = () => {
   const { drawer, cart } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
+
+  const showDrawer = () => {
+    dispatch({
+      type: "SET_VISIBLE",
+      payload: true,
+    });
+  };
+
+  const closeDrawer = () => {
+    dispatch({
+      type: "SET_VISIBLE",
+      payload: false,
+    });
+  };
+
   return (
-    <div>
-      <Drawer visible={true}>{JSON.stringify(cart)}</Drawer>
-    </div>
+    <>
+      <Button className="float-right" type="danger" onClick={showDrawer}>
+        {cart.length}
+      </Button>
+      <Drawer
+        visible={drawer}
+        title={`${cart.length} Items`}
+        width={350}
+        zIndex={1500}
+        keyboard={true}
+        onClose={closeDrawer}
+      >
+        {cart.map((p) => (
+          <div key={p._id} className="row">
+            <div className="col mb-3">
+              {p.images[0] ? (
+                <>
+                  <img src={p.images[0].url} width={"100%"} height={"auto"} />
+                  <p className="bg-primary text-light">
+                    <span className="pl-2">
+                      {p.name} x {p.count}
+                    </span>
+                  </p>
+                </>
+              ) : (
+                <>
+                  <img src={logo} width={"100%"} height={"auto"} />
+                  <p className="bg-primary text-light">
+                    <span className="pl-2">
+                      {p.name} x {p.count}
+                    </span>
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+        <Link
+          to={"/cart"}
+          className="btn btn-block btn-primary"
+          onClick={closeDrawer}
+        >
+          Go to Checkout
+        </Link>
+      </Drawer>
+    </>
   );
 };
 
