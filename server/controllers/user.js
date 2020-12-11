bec608cbc7ccf9c9565384d3f44d6b84bda4c57d
Workbench <workbench@Skybox.fios-router.home>
2020-12-11 15:15:08 -0500

    create server api method to save user cart in database

---
+++ server/controllers/user.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const Product = require("../models/Product");
+const Cart = require("../models/Cart");
+
+exports.userCart = async (req, res) => {
+  let products = [];
+  const { cart } = req.body;
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+  let existingCart = await Cart.findOne({ orderedBy: user.id }).exec();
+
+  if (existingCart) {
+    existingCart.remove();
+  }
+
+  for (let i = 0; i < cart.length; i++) {
+    let u = {};
+    u.product = cart[i]._id;
+    u.count = cart[i].count;
+    u.color = cart[i].color;
+
+    let { price } = await Product.findById(cart[i]._id).select("price").exec();
+    u.price = price;
+
+    products.push(u);
+  }
+
+  let totalAmount = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    totalAmount = totalAmount + products[i].price + products[i].count;
+  }
+
+  let c = await Cart({
+    products: products,
+    totalAmount: totalAmount,
+    orderedBy: { type: user._id },
+  }).save();
+
+  console.log("Saved:", c);
+  res.json({ ok: true });
+};
