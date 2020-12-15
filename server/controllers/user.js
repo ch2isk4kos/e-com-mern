504e8eb9d1c81536ccf531a6b5ce33d98422f8f2
Workbench <workbench@Skybox.fios-router.home>
2020-12-14 20:51:02 -0500

    add controller method to empty user cart

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -56,3 +56,10 @@ exports.getUserCart = async (req, res) => {
     totalAmountAfterDiscount: totalAfterDiscount,
   });
 };
+
+exports.emptyUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+  const cart = await Cart.findOneAndRemove({ orderedBy: user._id }).exec();
+
+  res.json(cart);
+};
