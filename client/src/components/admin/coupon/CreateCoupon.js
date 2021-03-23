c7ba726849097a8df88aa0eb2d8ee087b30ceafb
ch2isk4kos <ch2isk4kos@gmail.com>
2021-03-22 23:38:57 -0400

    comment out code for setup

--- client/src/components/admin/coupon/CreateCoupon.js
+++ client/src/components/admin/coupon/CreateCoupon.js
@@ -1,65 +1,65 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  getCoupons,
-  createCoupon,
-  removeCoupon,
-} from "../../../api/nodejs/coupons";
-import { toast } from "react-toastify";
-import { DeleteOutlined } from "@ant-design/icons";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+//   getCoupons,
+//   createCoupon,
+//   removeCoupon,
+// } from "../../../api/nodejs/coupons";
+// import { toast } from "react-toastify";
+// import { DeleteOutlined } from "@ant-design/icons";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
 const CreateCoupon = () => {
-  const { user } = useSelector((state) => ({ ...state }));
-  const [coupon, setCoupon] = useState([]);
+  //   const { user } = useSelector((state) => ({ ...state }));
+  //   const [coupon, setCoupon] = useState([]);
 
-  useEffect(() => {}, []);
+  //   useEffect(() => {}, []);
 
-  const loadCoupons = async () => {
-    const p = await getCoupons();
-    return setCoupons(p.data);
-  };
+  //   const loadCoupons = async () => {
+  //     const p = await getCoupons();
+  //     return setCoupons(p.data);
+  //   };
 
-  const handleOnChange = (e) => {
-    console.log({ [e.target.name]: e.target.value });
-    setCoupon({ ...coupon, [e.target.name]: e.target.value });
-  };
+  //   const handleOnChange = (e) => {
+  //     console.log({ [e.target.name]: e.target.value });
+  //     setCoupon({ ...coupon, [e.target.name]: e.target.value });
+  //   };
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+  //   const handleOnSubmit = (e) => {
+  //     e.preventDefault();
+  //     setIsLoading(true);
 
-    createCoupon(Coupon, user.token)
-      .then((res) => {
-        console.log(res.data);
-        window.alert(`Confirm ${res.data.name} Create`);
-        window.location.reload();
-        toast.success(`${res.data.name} succressfully created`);
-      })
-      .catch((err) => {
-        setIsLoading(true);
-        if (err.response.status === 400) toast.error(err.response.data.errMsg);
-      });
-  };
+  //     createCoupon(Coupon, user.token)
+  //       .then((res) => {
+  //         console.log(res.data);
+  //         window.alert(`Confirm ${res.data.name} Create`);
+  //         window.location.reload();
+  //         toast.success(`${res.data.name} succressfully created`);
+  //       })
+  //       .catch((err) => {
+  //         setIsLoading(true);
+  //         if (err.response.status === 400) toast.error(err.response.data.errMsg);
+  //       });
+  //   };
 
-  const handleOnDelete = async (slug) => {
-    if (window.confirm("Are You Sure?")) {
-      setIsLoading(true);
-      removeCoupon(slug, user.token)
-        .then((res) => {
-          setIsLoading(false);
-          loadProducts();
-          toast.error(`${res.data.name} Deleted`);
-        })
-        .catch((err) => {
-          if (err.response.status === 400) {
-            setIsLoading(true);
-            toast.error("DELETE ERROR:", err.response.data);
-          }
-        });
-    }
-  };
+  //   const handleOnDelete = async (slug) => {
+  //     if (window.confirm("Are You Sure?")) {
+  //       setIsLoading(true);
+  //       removeCoupon(slug, user.token)
+  //         .then((res) => {
+  //           setIsLoading(false);
+  //           loadProducts();
+  //           toast.error(`${res.data.name} Deleted`);
+  //         })
+  //         .catch((err) => {
+  //           if (err.response.status === 400) {
+  //             setIsLoading(true);
+  //             toast.error("DELETE ERROR:", err.response.data);
+  //           }
+  //         });
+  //     }
+  //   };
 
   return (
     <div className="container">
