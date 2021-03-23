a73e385a752dc1ca4ac8854b8306dd22d13e33f6
ch2isk4kos <ch2isk4kos@gmail.com>
2021-03-22 21:46:49 -0400

    add crud methods to coupon controller

---
+++ server/controllers/coupon.js
@@ -0,0 +1,29 @@
+const Coupon = require("../models/Coupon");
+
+exports.index = async (req, res) => {
+  try {
+    const list = await Coupon.find({}).sort({ createdAt: -1 }).exec();
+    res.json(list);
+  } catch (err) {
+    res.status(400).send("COUPON INDEX FAILED", err);
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    const { name, expiry, discount } = req.body;
+    res.json(await new Coupon({ name, parent, expiry, discount }).save());
+  } catch (err) {
+    console.log("COUPON CREATE", err);
+    res.status(400).send("CREATE COUPON FAILED");
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const s = await Coupon.findByIdAndDelete(req.params.couponId).exec();
+    res.json(s);
+  } catch (err) {
+    res.status(400).send("COUPON DELETE FAILED", err);
+  }
+};
