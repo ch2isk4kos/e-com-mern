109028df3def85c145cbd8d2c69d0a03ce2199b9
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-09 17:13:21 -0400

    create template for payment component

---
+++ client/src/components/cart/Payment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Payment = () => {
+  return (
+    <div className="container p-5 text-center">
+      <h3>Stripe Payment</h3>
+    </div>
+  );
+};
+
+export default Payment;
