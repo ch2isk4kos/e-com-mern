6241fd5f949ba6d074a8094db4d32711a4de03ea
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-10 17:21:29 -0400

    pass coupon attribute to server method to access data for purchase amount calculations on back end

--- client/src/api/nodejs/stripe.js
+++ client/src/api/nodejs/stripe.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const NODE_API = process.env.REACT_APP_NODE_API_URL;
 
-export const createPaymentIntent = async (token) => {
+export const createPaymentIntent = async (token, coupon) => {
   return await axios.post(
     `${NODE_API}/create-payment-intent`,
-    {},
+    { isCouponApplied: coupon },
     {
       headers: {
         auth: token,
