5bd090cb2f4687eef003f0dec5b3517e9c4b274c
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-09 20:25:13 -0400

    add stripe interfact to payment component

--- client/src/components/cart/Payment.js
+++ client/src/components/cart/Payment.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import StripePayment from "./StripePayment";
+import "../stripe.css";
+
+//load stripe outside of Payment component to avoid recreating stripe object on each render
+const promise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY);
 
 const Payment = () => {
   return (
     <div className="container p-5 text-center">
       <h3>Stripe Payment</h3>
+      <Elements stripe={promise}>
+        <div className="col-md-8 offset-md-2">
+          <StripePayment />
+        </div>
+      </Elements>
     </div>
   );
 };
