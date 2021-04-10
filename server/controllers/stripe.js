b362eb5741b79bd334ef121ce40c182c8b4d1a46
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-10 17:18:13 -0400

    apply functionality to charge discounted payment via stripe transaction if coupon has been applied to user cart

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,23 +5,41 @@ const Coupon = require("../models/Coupon");
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
-  //apply coupon
-  //calculate price
+  console.log("Request Body: ", req.body);
+  console.log("Coupon Applied: ", req.body.isCouponApplied);
+
+  const { isCouponApplied } = req.body;
 
   //find user
   const user = await User.findOne({ email: req.user.email }).exec();
-  //get total amount of user cart
+  //get total amount from all items in user cart
   const cartItems = await Cart.findOne({ orderedBy: user._id }).exec();
 
   console.log("Cart Items: ", cartItems);
 
+  const totalAfterDiscount = cartItems.totalAfterDiscount;
+  console.log("Total After Discount: ", totalAfterDiscount);
+
+  const totalAmount = cartItems.totalAmount;
+  console.log("Total Amount: ", totalAmount);
+
+  let purchaseAmount = 0;
+
+  if (isCouponApplied && totalAfterDiscount) {
+    purchaseAmount = Math.round(totalAfterDiscount * 100);
+  } else {
+    purchaseAmount = Math.round(totalAmount * 100);
+  }
+
   //create payment intent with order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: cartItems.totalAmount * 100,
+    amount: purchaseAmount,
     currency: "usd",
   });
 
   res.send({
     clientSecret: paymentIntent.client_secret,
+    items: cartItems,
+    payable: purchaseAmount,
   });
 };
