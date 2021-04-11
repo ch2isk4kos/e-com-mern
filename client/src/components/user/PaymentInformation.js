33062986abf543e2404537232bd85f86b4d02766
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-11 18:51:05 -0400

    create functional component as a template for user payment information per each user order

---
+++ client/src/components/user/PaymentInformation.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const PaymentInformation = ({ order }) => {
+  return (
+    <div>
+      <p>
+        <span>
+          <b>Order ID: </b>
+          {order.paymentIntent.id}
+        </span>
+      </p>
+      <p>
+        <span>
+          <b>Order Date: </b>
+          {new Date(order.paymentIntent.created * 1000).toLocaleString()}
+        </span>
+      </p>
+      <p>
+        <span>
+          <b>Amount: </b>
+          {(order.paymentIntent.amount / 100).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </span>{" "}
+        <span>{order.paymentIntent.currency.toUpperCase()}</span>
+      </p>
+      <p>
+        <span>
+          <b>Payment Method: </b>
+          {order.paymentIntent.payment_method_types[0]}
+        </span>
+      </p>
+      <p>
+        <span>
+          {order.paymentIntent.status === "succeeded" ? (
+            <p>
+              <b>Transaction Status: </b>
+              <span className="badge bg-success text-white">
+                {order.paymentIntent.status}
+              </span>
+            </p>
+          ) : (
+            <p>
+              <b>Transaction Status: </b>
+              <span className="badge bg-danger text-white">
+                {order.paymentIntent.status}
+              </span>
+            </p>
+          )}
+        </span>
+      </p>
+      {order.orderStatus === "Not Processed" ? (
+        <p>
+          <b>Order Status: </b>
+          <span className="badge bg-danger text-white">
+            {order.orderStatus}
+          </span>
+        </p>
+      ) : (
+        <p>
+          <b>Order Status: </b>
+          <span className="badge bg-success text-white">
+            {order.orderStatus}
+          </span>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default PaymentInformation;
