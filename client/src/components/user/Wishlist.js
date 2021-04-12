053d631f30df42dc16b305f374406004313b46ec
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-12 18:07:34 -0400

    add functionality to add product to user wish list from product card

--- client/src/components/user/Wishlist.js
+++ client/src/components/user/Wishlist.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "./UserNav";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserWishlist, updateUserWishlist } from "../../api/custom/user";
+import { toast } from "react-toastify";
 
 const Wishlist = () => {
+  const [wishlist, setWishlist] = useState([]);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadWishlist();
+  }, []);
+
+  const loadWishlist = () => {
+    getUserWishlist(user.token).then((res) => {
+      console.log(res.data, null, 4);
+      // setWishlist(res.data);
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-2">
+        <div className="col-md-4">
           <UserNav />
         </div>
-        <div className="col">
+        <div className="col-md-6">
           <h1>Wishlist</h1>
         </div>
       </div>
