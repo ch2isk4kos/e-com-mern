2dd61161e9242522b6aaf191fe1f3d244081044e
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-12 16:01:00 -0400

    render product table in admin order dashboard

--- client/src/components/admin/AdminOrders.js
+++ client/src/components/admin/AdminOrders.js
@@ -3,6 +3,49 @@ import PaymentInformtion from "../user/PaymentInformation";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 const AdminOrders = ({ orders, handleOnOrderUpdate }) => {
+  const renderPaymentTable = (order) => (
+    <table className="table table-bordered mt-2">
+      <thead className="thead-light">
+        <tr className="bg-dark text-white">
+          <th scope="col">Product</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody style={{ backgroundColor: "White" }}>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.name}</b>
+            </td>
+            <td>${p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.product.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined
+                  style={{
+                    color: "green",
+                  }}
+                />
+              ) : (
+                <CloseCircleOutlined
+                  style={{
+                    color: "red",
+                  }}
+                />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
   return (
     <>
       {orders.map((o) => (
@@ -11,6 +54,7 @@ const AdminOrders = ({ orders, handleOnOrderUpdate }) => {
             <div className="row mt-3">
               <PaymentInformtion order={o} currentStatus={false} />
             </div>
+            {renderPaymentTable(o)}
             <div className="row mb-3">
               <div className="col-md-12">
                 <p>
