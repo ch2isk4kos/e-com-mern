cc7f662665693c9307ca6e9cea0cccb99fd7992d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-12 00:37:42 -0400

    render options for order status update

--- client/src/components/admin/AdminOrders.js
+++ client/src/components/admin/AdminOrders.js
@@ -8,6 +8,24 @@ const AdminOrders = ({ orders, handleOnOrderUpdate }) => {
       {orders.map((o) => (
         <div key={o._id} className="row pb-5">
           <PaymentInformtion order={o} />
+
+          <div className="row">
+            <div className="col-md-12">
+              <p>
+                <b>Update Status:</b>
+              </p>
+
+              <select
+                onChange={(e) => handleOnOrderUpdate(o._id, e.target.value)}
+              >
+                <option value="Not Processed">Not Processed</option>
+                <option value="Processing">Processing</option>
+                <option value="Dispatch">Dispatch</option>
+                <option value="Cancelled">Cancelled</option>
+                <option value="Completed">Completed</option>
+              </select>
+            </div>
+          </div>
         </div>
       ))}
     </>
