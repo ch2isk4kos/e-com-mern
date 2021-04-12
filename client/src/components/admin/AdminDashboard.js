14a5aa437c518fde13dfe3305b58409b3262847a
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-11 23:44:37 -0400

    create admin functionality that fetches all user orders from database

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import AdminNav from "./AdminNav";
+import {
+  getAdminOrders,
+  updateAdminOrderStatus,
+} from "../../api/nodejs/admin.js";
+import { toastify } from "react-toastify";
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+
+  const loadUserOrders = () => {
+    getAdminOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -10,6 +31,7 @@ const AdminDashboard = () => {
         </div>
         <div className="col">
           <h1>Admin Dashboard</h1>
+          {JSON.stringify(orders)}
         </div>
       </div>
     </div>
