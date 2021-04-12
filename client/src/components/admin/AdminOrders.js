356c484a4bb2bd257086431f78dea4a87cd24d3f
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-11 23:59:25 -0400

    create order template to render for admin

---
+++ client/src/components/admin/AdminOrders.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminOrders = ({ orders }) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrders;
